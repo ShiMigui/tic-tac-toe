a538e09218bc3b22bb12d625ce400ac689c9c758
= <=>
2024-06-10 23:17:22 -0300

    :sparkles: feat: add TicTacToe.js

---
+++ assets/scripts/TicTacToe.js
@@ -0,0 +1,57 @@
+export default class TicTacToe {
+    constructor(squares = [], currentPlayer = '') {
+        this.setGameProps(squares, currentPlayer);
+    }
+
+    set currentPlayer(currentPlayer = '') {
+        currentPlayer = currentPlayer.toLowerCase().trim();
+
+        if (currentPlayer != 'x' && currentPlayer != 'o' && currentPlayer != null) {
+            throw new Error(`Value of current player must be equals to 'x', 'o' or null! value = ${currentPlayer}`);
+        }
+
+        this._currentPlayer = currentPlayer;
+    }
+    get currentPlayer() { return this._currentPlayer; }
+
+    set squares(squares = []) {
+        if (squares.length != 9)
+            throw new Error(`Board's squares number must be equal to 9! length: ${squares.length}`);
+
+        this._squares = squares;
+    }
+    get squares() { return this._squares; }
+
+    setGameProps(squares = [], currentPlayer = '') {
+        this.squares = squares;
+        this.currentPlayer = currentPlayer;
+    }
+
+    restart() {
+        this.squares.fill(null);
+        this.currentPlayer = 'x';
+    }
+
+    victory() {
+        for (let position of [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]) {
+            let first = this.squares[position[0]];
+            if (first !== null && this.squares[position[1]] == first && this.squares[position[2]] == first) {
+                return first;
+            }
+        }
+        return null;
+    }
+
+    nextPlayer() {
+        this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
+    }
+
+    selectedSquare(squareIndex = 0) {
+        if (squareIndex < 0 || squareIndex > 8) {
+            throw new Error(`square index must be between 0 and 8! square index = ${squareIndex}`);
+        }
+
+        this.squares[squareIndex] = this.currentPlayer;
+        this.nextPlayer();
+    }
+}
--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,44 +1,49 @@
-const $squares = document.querySelectorAll('.square'), $msg = document.getElementById('current-player');
-let _board = [], _q = '', _running = false;
+const game = () => {
+    const $squares = document.querySelectorAll('.square');
+    const $currentPlayer = document.getElementById('current-player');
+    let _board = [], _q = '', _running = false;
 
-$squares.forEach((el, i) => el.addEventListener('click', () => setSquareClicked(i)));
-document.getElementById('restart').addEventListener('click', start);
+    $squares.forEach((el, i) => el.addEventListener('click', () => setSquareClicked(i)));
+    document.getElementById('restart').addEventListener('click', start);
 
-function start() {
-    $squares.forEach((el) => el.setAttribute('q', ''))
-    _board = ['', '', '', '', '', '', '', '', ''];
-    _running = true;
-    syncQuery('x');
-}
-
-function setQuery(el) { el.setAttribute('q', _q) }
-
-function setSquareClicked(i) {
-    if (!_running || _board[i]) return;
-
-    setQuery($squares[i]);
-    _board[i] = _q;
-
-    if (verifyVictory()) {
-        alert(`Victorious: ${_q}`);
-        _running = false;
-        return;
+    function start() {
+        $squares.forEach((el) => el.setAttribute('q', ''))
+        _board = ['', '', '', '', '', '', '', '', ''];
+        _running = true;
+        syncQuery('x');
     }
 
-    syncQuery(_q == 'x' ? 'o' : 'x');
-}
+    function setQuery(el) { el.setAttribute('q', _q) }
 
-function syncQuery(q) {
-    $msg.innerText = q.toUpperCase();
-    _q = q;
-}
+    function setSquareClicked(i) {
+        if (!_running || _board[i]) return;
 
-function verifyVictory() {
-    for (let s of [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]) {
-        let first = _board[s[0]];
-        if (first !== '' && _board[s[1]] == first && _board[s[2]] == first) return true;
+        setQuery($squares[i]);
+        _board[i] = _q;
+
+        if (verifyVictory()) {
+            alert(`Victorious: ${_q}`);
+            _running = false;
+            return;
+        }
+
+        syncQuery(_q == 'x' ? 'o' : 'x');
     }
-    return false;
+
+    function syncQuery(q) {
+        $currentPlayer.innerText = q.toUpperCase();
+        _q = q;
+    }
+
+    function verifyVictory() {
+        for (let s of [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]) {
+            let first = _board[s[0]];
+            if (first !== '' && _board[s[1]] == first && _board[s[2]] == first) return true;
+        }
+        return false;
+    }
+
+    return start;
 }
 
-start();
+game()();
